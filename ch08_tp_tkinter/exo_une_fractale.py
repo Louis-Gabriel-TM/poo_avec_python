9a7b5d278eb215a6bad848a7abb2ad86f3e84dfc
Louis-Gabriel-TM <louis.gabriel@orange.fr>
2019-01-10 12:42:27 +0100

    Add comment to 'une fractalee' exercise

--- ch08_tp_tkinter/exo_une_fractale.py
+++ ch08_tp_tkinter/exo_une_fractale.py
@@ -12,26 +12,36 @@ def plot(x, y):
     )
 
 def init_vertex():
+    """Vide la liste 'vertex' (globale) et y place trois nouveaux sommets dont les coordonnées (x, y) ont été fixées de manière aléatoire.
+    La zone de dessin est effacée au passage.
+    """
+    # On importe une variable globale pour la modifier : c'est mal :-p
     global vertex
+    # On efface la zone de dessin...
     canvas.delete(ALL)
+    # ... et on vide la liste des sommets
     vertex = []
+
     for _ in range(3):
-        x = randrange(600)
+        x = randrange(600)  # randrange(600) renvoie un entier entre 0 et 599 inclus
         y = randrange(600)
-        vertex.append((x, y))
-        plot(x, y)
+        vertex.append((x, y))  # on ajoute un sommet (x, y) à la liste...
+        plot(x, y)  # ... et on l'affiche
 
 def plot_process():
-    pt1 = choice(vertex)
+    """Effectue 50 itérations de la boucle décrite dans l'énoncé.
+    """
+    pt1 = choice(vertex)  # 'choice' prend un élément au hasard dans la liste 'vertex'
     for _ in range(50):
         pt2 = choice(vertex)
+        # on calcule les coordonnées du milieu du segment joignant 'pt1' à 'pt2'
         middle = [
             (pt1[0] + pt2[0]) // 2,
             (pt1[1] + pt2[1]) // 2
         ]
         plot(middle[0], middle[1])
         #canvas.update_idletasks()
-        pt1 = middle
+        pt1 = middle  # on va utiliser ce milieu comme point de départ pour le prochain tour de boucle
 
 ### Programme principal ###
 
@@ -41,8 +51,10 @@ vertex = []
 # On créé la fenêtre, la zone de dessin et les deux boutons
 window = Tk()
 window.title("Triangle de Sierpinski")
+
 canvas = Canvas(window, width=600, height=600, bg='ivory')
 canvas.pack(side=TOP, padx=5, pady=5)
+
 btn_init = Button(window, text="Init", command=init_vertex)
 btn_init.pack(side=LEFT, padx=5, pady=5)
 btn_go = Button(window, text="50 iterations", command=plot_process)
